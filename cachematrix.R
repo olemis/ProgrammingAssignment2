72cd661e3c587cf8c2da8129c2dae421789a8da0
Olemis Lang <olemis+trac@gmail.com>
2014-06-10 18:42:22 -0400

    [olemis] Cached matrix implementation.
    P.S. Interactive session recorded in cachematrix.log

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache potentially time-consuming computation
 
-## Write a short comment describing this function
-
+## makeVector creates a special "vector", which is really a list 
+## containing functions to 
+#
+## - set the value of the matrix
+## - get the value of the matrix
+## - set the value of the inverse matrix
+## - get the value of the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculate the inverse matrix of the special "vector"
+## created with the above function.
+## However, it first checks to see if it has already
+## been calculated. If so, it gets the value from the cache
+## and skips the computation. Otherwise, it calculates
+## the inverse matrix of the data and sets the value of the value
+## in the cache via the setinverse function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
